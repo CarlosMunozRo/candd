2d7a5ee5bb87d0bf0598a0dca0d95904ee25b72c
Carlos Muñoz <carmuny54@gmail.com>
2026-01-30 22:18:30 +0100

    Added astro db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,16 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Song = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    title: column.text(),
+    artist: column.text(),
+    url: column.text(),
+    dateAdded: column.date(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Song }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,19 @@
-import { db } from 'astro:db';
+import { db, Song } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Song).values([
+	{
+	  title: 'Song Title 1',
+	  artist: 'Artist 1',
+	  url: 'https://example.com/song1',
+	  dateAdded: new Date(),
+	},
+	{
+	  title: 'Song Title 2',
+	  artist: 'Artist 2',
+	  url: 'https://example.com/song2',
+	  dateAdded: new Date(),
+	},
+  ]);
 }
