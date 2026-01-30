b64c130fa7f0db80f95327b9924106f026a35a5b
Carlos Muñoz <carmuny54@gmail.com>
2026-01-30 23:31:47 +0100

    Added carousel with songs

--- db/seed.ts
+++ db/seed.ts
@@ -4,16 +4,34 @@ import { db, Song } from 'astro:db';
 export default async function seed() {
   await db.insert(Song).values([
 	{
-	  title: 'Song Title 1',
-	  artist: 'Artist 1',
-	  url: 'https://example.com/song1',
-	  dateAdded: new Date(),
+	  title: 'POWER',
+	  artist: 'Kanye West',
+	  url: 'spotify:track:2gZUPNdnz5Y45eiGxpHGSc',
+	  dateAdded: new Date('2026-01-30'),
 	},
 	{
-	  title: 'Song Title 2',
-	  artist: 'Artist 2',
-	  url: 'https://example.com/song2',
-	  dateAdded: new Date(),
+	  title: 'All of the Lights',
+	  artist: 'Kanye West',
+	  url: 'spotify:track:22L7bfCiAkJo5xGSQgmiIO',
+	  dateAdded: new Date('2026-01-29'),
 	},
+	{
+	  title: 'Monster',
+	  artist:'Kanye West',
+	  url: 'spotify:track:1ZHYJ2Wwgxes4m8Ba88PeK',
+	  dateAdded: new Date('2026-01-28'),
+	},
+	{
+	  title: 'Runaway',
+	  artist:'Kanye West',
+	  url: 'spotify:track:3DK6m7It6Pw857FcQftMds',
+	  dateAdded: new Date('2026-01-27'),
+	},
+	{
+	  title: 'Devil in a New Dress',
+	  artist:'Kanye West',
+	  url: 'spotify:track:1UGD3lW3tDmgZfAVDh6w7r',
+	  dateAdded: new Date('2026-01-26'),
+	}
   ]);
 }
